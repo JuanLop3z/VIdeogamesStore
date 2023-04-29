355bd2359a0683bf109b857b53a4f7d042fa72cb
Juan Sebastian Lopez Galindo <juan.lopez.galindo@pi.edu.co>
2023-04-28 22:30:38 -0500

    Entrega final

--- SQL/POBLACION_TABLAS.sql
+++ SQL/POBLACION_TABLAS.sql
@@ -1,12 +1,13 @@
 --DB A USAR--
 use RetoSophos
 go
-
+--Variables pre-declaradas--
+declare @Registrado bit, @Mensaje varchar(100)
 
 -------------------------PROCESOS DE POBLACION DE TABLAS-----------------------------
 
 --POBLAR LA TABLA CLIENTES--
-declare @Registrado bit, @Mensaje varchar(100)
+
 
 exec sp_CrearClientes '1020712021', 'Marco Antonio Rojas Cruz', 15 , @Registrado output, @Mensaje output
 exec sp_CrearClientes '1020845124', 'Juan Anselmo Vera Cruz', 30 , @Registrado output, @Mensaje output
@@ -20,7 +21,6 @@ select @Mensaje
 
 
 --POBLAR LA TABLA USUARIOS--
-declare @Registrado bit, @Mensaje varchar(100)
 
 exec sp_RegistrarUsuario 'Antonio', '03AC674216F3E15C761EE1A5E255F067953623C8B388B4459E13F978D7C846F4',@Registrado output, @Mensaje output
 
@@ -37,7 +37,7 @@ exec sp_ValidarUsuario 'Antonio','03AC674216F3E15C761EE1A5E255F067953623C8B388B4
 
 
 --POBLAR LA TABLA JUEGOS--
-declare @Registrado bit, @Mensaje varchar(100)
+
 
 exec sp_CrearJuego 'Assassins Creed 2', '347354', 'PS4/PC/Xbox 360/macOS','Patrick Plourde/Olivier Palmieri','Ezio Auditore','Ubisoft Montreal/Ubisoft Kyiv','19/11/2009', @Registrado output, @Mensaje output
 exec sp_CrearJuego 'Grand Theft Auto V', '454320', 'PS4/PS5/Xbox One/Xbox 360/PS3/PC','Leslie Benzies/Imran Sarwar','Michael/Trevor/Franklin','Rockstar Games/Rockstar North','17/09/2013', @Registrado output, @Mensaje output
@@ -54,7 +54,7 @@ select @Mensaje
 
 
 --ACTUALIZAR LA TABLA DE JUEGOS(Precio)--
-declare @Registrado bit, @Mensaje varchar(100)
+
 
 exec sp_ActualizarPrecio 'Assassins Creed 2',  347500, @Registrado output, @Mensaje output
 --Mensajes de salida--
@@ -63,7 +63,6 @@ select @Mensaje
 
 
 --REGISTRAR EL ALQUILER--
-declare @Registrado bit, @Mensaje varchar(100)
 
 exec sp_RegistrarAlquiler '1020712021','Assassins Creed 2', @Registrado output, @Mensaje output
 exec sp_RegistrarAlquiler '1020845124','Grand Theft Auto V', @Registrado output, @Mensaje output
